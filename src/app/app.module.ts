2ef1b0918c97172ebd07c80122e2f01bae6f2503
PedroHSMoreira <moreiraphs@outlook.com>
2019-10-31 22:55:18 -0300

    ADD.: after effects with lottie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 
+import { LottieModule } from 'ngx-lottie';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -14,6 +16,11 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+
+
+export function playerFactory() {
+  return import('lottie-web');
+}
 
 @NgModule({
   declarations: [
@@ -32,7 +39,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'mt-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+options: AnimationOptions = {
+  path: '../assets/animations/cooking.json',
+  renderer: 'canvas',
+  autoplay: true,
+  loop: false
+}
+animationCreated(animationItem: AnimationItem): void {
+  console.log(animationItem);
+} 
+
+styles: Partial<CSSStyleDeclaration> = {
+  maxWidth: '300px',
+  margin: '0 auto'
+};
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
