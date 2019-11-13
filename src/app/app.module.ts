220b2f033adc515a75ce4de2f0c644f24ecd112d
PedroHSMoreira <moreiraphs@outlook.com>
2019-11-12 21:26:11 -0300

    ADD.: ngModel on forms in order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { LottieModule } from 'ngx-lottie';
 
@@ -42,6 +43,7 @@ export function playerFactory() {
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
