36f262b6a3b5447a45c1e6bffe736977cdbf24e8
Pedro Moreira <moreiraphs@outlook.com>
2020-01-12 22:27:21 -0300

    ADD.: middlewares for auth in backend

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from 'express'
 import { User, users } from './user'
 
+const isValid = (user: User): boolean => {
+    if (!user) {
+        return false
+    }
+    const dbUser: User = users[user.email]
+    return dbUser != undefined && dbUser.matches(user)
+}
+
 class AuthMiddleware {
     public handleAuthentication(req: Request, res: Response) {
         const user: User = req.body
-        if(this.isValid(user)) {
-
+        if (isValid(user)) {
+            const dbUser: User = users[user.email]
+            res.send({ name: dbUser.name, email: dbUser.email })
         } else {
-            res.status(403).send({message: 'Dados inválidos.'})
+            res.status(403).send({ message: 'Dados inválidos.' })
         }
-    }
-    
-    private isValid(user: User): boolean {
-        if(!user) {
-            return false
-        }
-        const dbUser = users[user.email]
-        return dbUser != undefined && dbUser.matches(user)
     }
 }
 
