039e790ce4b6c8441ce8024b25662a0c5b425e06
Pedro Moreira <moreiraphs@outlook.com>
2020-01-14 11:10:51 -0300

    FIX.: using zones in handle error

--- src/app/app-error-handler.ts
+++ src/app/app-error-handler.ts
@@ -1,33 +1,35 @@
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, NgZone } from '@angular/core';
 import { HttpErrorResponse } from "@angular/common/http";
 import { NotificationService } from './core/notification.service';
 import { LoginService } from './core/login.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private ns: NotificationService, private loginService: LoginService) {
+    constructor(private ns: NotificationService, private loginService: LoginService, private zone: NgZone) {
         super()
     }
 
     handleError(errorReponse: HttpErrorResponse | any) {
         if (errorReponse instanceof HttpErrorResponse) {
             const message = errorReponse.error.message
-            switch (errorReponse.status) {
-                case 401:
-                    this.ns.notify(message || 'Não autorizado')
-                    this.loginService.handleLogin()
-                    break;
-                case 403:
-                    this.ns.notify(message || 'Não autorizado.')
-                    break;
-                case 404:
-                    this.ns.notify(message || 'Recurso não encontrado.')
-                    break;
-                case 500:
-                    this.ns.notify(message || 'Erro interno.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch (errorReponse.status) {
+                    case 401:
+                        this.ns.notify(message || 'Não autorizado')
+                        this.loginService.handleLogin()
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado.')
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Recurso não encontrado.')
+                        break;
+                    case 500:
+                        this.ns.notify(message || 'Erro interno.')
+                        break;
+                }
+            })
         }
         super.handleError(errorReponse)
     }
